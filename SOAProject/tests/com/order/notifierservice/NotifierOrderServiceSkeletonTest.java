e9699591766cb03a79c381521d19f879284d8a2b
Jochem Harmes <jochemharmes@gmail.com>
2019-03-24 14:21:34 +0100

    Updated WSDL -> price to double and removed unused message

--- SOAProject/tests/com/order/notifierservice/NotifierOrderServiceSkeletonTest.java
+++ SOAProject/tests/com/order/notifierservice/NotifierOrderServiceSkeletonTest.java
@@ -1,13 +1,38 @@
 package com.order.notifierservice;
 
+import com.order.elements.FindDriverRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NotifierOrderServiceSkeletonTest {
 
+    private FindDriverRequest request;
+    private NotifierOrderServiceSkeleton skeleton;
+
+
+    @BeforeEach
+    void beforeEach(){
+        this.request = new FindDriverRequest();
+
+        this.skeleton = new NotifierOrderServiceSkeleton();
+    }
 
     @Test
     void findDriver() {
+
+    }
+
+    @Test
+    void findDriverInvalidLocation() {
+    }
+
+    @Test
+    void findDriverInvalidPrice() {
+    }
+
+    @Test
+    void findDriverInvalidCustomer() {
     }
 }
